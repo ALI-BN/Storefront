391a62fedf4f5456ab94cd79d3427efa431bf6fd
ALI-BN <95654803+ALI-BN@users.noreply.github.com>
2022-06-29 15:50:15 +0300

    Login page view

--- storefront/urls.py
+++ storefront/urls.py
@@ -14,14 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.views.generic import TemplateView
+# from users import views as user_views     # this is instead of making the regular actions
 import debug_toolbar
 
 urlpatterns = [
+    # path('register/', user_views.register, name = 'register'),
     path('admin/', admin.site.urls),
     path('', include('playground.urls')),
     path('register/', include('users.urls')),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('__debug__/', include('debug_toolbar.urls')),
     path('p/', TemplateView.as_view(template_name='index.html'))
 ]
